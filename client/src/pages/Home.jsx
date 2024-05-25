8da3404c0064170fa3c0dff6dcc9f914f64d71ae
eye97 <parampawar418@gmail.com>
2024-05-25 10:53:46 +0530

    socket is working and it's 24!

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,16 +1,28 @@
-import React from "react";
-import '../App.css';
+import React, { useEffect } from "react";
+import "../App.css";
+import { useSocket } from "../providers/Socket";
 
 const Homepage = () => {
-	return(
-		<div className="homepage-container">
-			<div className="input-container">			
-				<input type="email" placeholder="enter your email"/>
-				<input type="text" placeholder="enter your Code"/>
-				<button>Enter Room</button>
-			</div>
-		</div>
-	)
-	}
+  const { socket } = useSocket();
 
-export default Homepage
+  useEffect(() => {
+    if (socket) {
+      console.log("Emitting join-room event");
+      socket.emit("join-room", { RoomId: "1", EmailId: "anything@gmail.com" });
+    } else {
+      console.log("Socket not defined yet");
+    }
+  }, [socket]);
+
+  return (
+    <div className="homepage-container">
+      <div className="input-container">
+        <input type="email" placeholder="Enter your email" />
+        <input type="text" placeholder="Enter your code" />
+        <button>Enter Room</button>
+      </div>
+    </div>
+  );
+};
+
+export default Homepage;
--- server/index.js
+++ server/index.js
@@ -1,18 +1,26 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const {Server} = require('socket.io')
+const express = require("express");
+const bodyParser = require("body-parser");
+const { Server } = require("socket.io");
 
-const io = new Server ();
+const io = new Server({
+  cors: true,
+});
 const app = express();
- 
 
 app.use(bodyParser.json());
 
+const EmailIdtoSocketMapping = new Map();
+
 io.on("connection", (socket) => {
-	
+  console.log("New connection:", socket.id);
+  socket.on("join-room", (data) => {
+    const { RoomId, EmailId } = data;
+    console.log("User", EmailId, "Joined Room", RoomId);
+    EmailIdtoSocketMapping.set(EmailId, socket);
+    socket.join(RoomId);
+    socket.broadcast.to(RoomId).emit("user-joined", { EmailId });
+  });
 });
 
-
-app.listien(8000,() => console.log("Http server running at 8000 port"));
-io.listien(8001);
-
+app.listen(8000, () => console.log("HTTP server running on port 8000"));
+io.listen(8001, () => console.log("Socket.IO server running on port 8001"));
